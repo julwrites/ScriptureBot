50dc3500b717c97f9fc2fc7927305ce0cf4d0f32
julwrites <jump4jupiter@gmail.com>
2017-08-23 00:32:44 +0800

    Trying to preserve tags as much as possible

--- common/html_utils.py
+++ common/html_utils.py
@@ -54,7 +54,7 @@ def to_sup(text):
 
 def foreach_tag(soup, tags, fn):
     for tag in soup.select(tags):
-        tag.string = fn(tag.text)
+        tag.replace_with(fn(tag.text))
 
 def foreach_header(soup, fn):
     foreach_tag(soup, soupify_tags(HTML_HEADER_TAGS), fn)
