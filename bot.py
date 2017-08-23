0f34daf3d61fe11a1663fc7eedc17afdaacbc245
julwrites <jump4jupiter@gmail.com>
2017-08-24 00:06:36 +0800

    Start runs on every single message, and will silently update any user info changes

--- bot.py
+++ bot.py
@@ -27,38 +27,29 @@ CMD_START_PROMPT = 'Hello {}, I\'m Biblica! I hope I will be helpful as a tool f
 
 # This is a special command, specialized to this bot
 def cmd_start(cmd, msg):
-    if cmd == CMD_START:
+    # Register User
+    user_json = msg.get('from')
+    uid = get_uid(user_json.get('id'))
+    user = get_user(uid)
+
+    # This runs to update the user's info, or register
+    if user_json is not None:
+        set_profile(
+            user_json.get('id'), 
+            user_json.get('username'), 
+            user_json.get('first_name'), 
+            user_json.get('last_name'))
+
+    # If this is the user's first time registering
+    if user is None:
         debug.log_cmd(cmd)
-
-        # Register User
-        user_json = msg.get('from')
-
-        # Read the user to echo back
-        uid = get_uid(user_json.get('id'))
         user = get_user(uid)
 
-        # This runs to update the user's info, or register
-        if user_json is not None:
-            debug.log(str(user_json.get('first_name'))
-            + ' ' + str(user_json.get('last_name'))
-            + ': ' + str(user_json.get('username'))
-            + ' - ' + str(user_json.get('id')))
-
-            set_profile(
-                user_json.get('id'), 
-                user_json.get('username'), 
-                user_json.get('first_name'), 
-                user_json.get('last_name'))
-
-        # If this is not the user's first time registering
-        if user is None:
-            user = get_user(uid)
-
-            telegram.send_msg(CMD_START_PROMPT.format(user.get_name_string()), user.get_uid())
-            debug.log('Registering ' + user.get_name_string())
+        telegram.send_msg(CMD_START_PROMPT.format(user.get_name_string()), user.get_uid())
+        debug.log('Registering ' + user.get_name_string())
 
         return True
-    
+
     return False
 
 class BotHandler(webapp2.RequestHandler):
