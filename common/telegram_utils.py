d128fa1adb4b1ccf674df8a5dc5ac0c736f7f34a
julwrites <jump4jupiter@gmail.com>
2017-08-25 16:21:00 +0800

    Sending the user class to the hook

--- common/telegram_utils.py
+++ common/telegram_utils.py
@@ -18,7 +18,7 @@ def foreach_user(fn):
 
     try:
         for user in query.run(batch_size=500):
-            fn(user)
+            fn(get_user(get_uid(user)))
     except Exception as e:
         debug.log(str(e))
  
