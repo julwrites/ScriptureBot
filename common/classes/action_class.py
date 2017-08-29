f2deb98e7e0340bb01ccd7aca14b9b15baf2e6b5
julwrites <jump4jupiter@gmail.com>
2017-08-30 00:11:40 +0800

    Generalizing the Match function a bit more for possible future extensions, and removing unnecessary code

--- common/classes/action_class.py
+++ common/classes/action_class.py
@@ -7,23 +7,31 @@ from common import debug
 
 # Defines an interface for all functionality that can be executed by the bot
 class Action():
-    # Do not overwrite if possible, this checks the message text against the command name
-    def match(self, msg_text):
-        return msg_text.find(self.identifier()) != -1
-
     # To be inherited and overwritten with a check for whether this is waiting for a response
     def waiting(self, user):
-        return user.get_state() == self.identifier()
+        if user.get_state() == self.identifier():
+            debug.log('Waiting for ' + self.identifier())
+            return True
+        return False
 
     # Do not overwrite if possible, this performs basic checks and resolves the action
     def execute(self, user, msg):
         try:
             if user is not None:
-                if self.match(msg.get('text').strip()) or self.waiting(user):
+                if self.match(msg) or self.waiting(user):
                     return self.resolve(user, msg)
         except:
             debug.log('Execute failed! ' + self.identifier())
             return False
+
+    # Do not overwrite if possible, this checks the message text against the command name
+    def match(self, msg):
+        msg_text = msg.get('text').strip() 
+        if msg_text.find(self.identifier()) != -1:
+            debug.log('Matched with ' + self.identifier())
+            return True
+        return False
+
 
 
     # To be inherited and overwritten with the name of this action
