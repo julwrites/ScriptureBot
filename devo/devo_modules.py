0df72be0a05ed2dbbae18d7cc912308b97f249d7
Julian Teh <julian.teh@autodesk.com>
2017-09-07 17:27:53 +0800

    Subscribe action

--- devo/devo_modules.py
+++ devo/devo_modules.py
@@ -1,10 +1,12 @@
 
 # Local modules
+from devo import subscribe_actions
 from devo import mcheyne_actions, mcheyne_hooks
 from devo import cac_hooks
 
 def get_actions():
-    return mcheyne_actions.get()
+    return subscribe_actions.get() + \
+            mcheyne_actions.get()
 
 def get_hooks():
     return mcheyne_hooks.get()
