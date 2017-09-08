e8d1d206255cab92c5a7e308f035ffa4bee6b82a
Julian Teh <julian.teh@autodesk.com>
2017-09-08 14:37:46 +0800

    Renaming variables

--- bible/bgw_utils.py
+++ bible/bgw_utils.py
@@ -43,24 +43,24 @@ def fetch_bgw(query, version='NIV'):
 def get_passage_raw(ref, version='NIV'):
     debug.log('Querying for passage ' + ref)
 
-    passageSoup = fetch_bgw(ref, version)
-    if passageSoup is None:
+    soup = fetch_bgw(ref, version)
+    if soup is None:
         return None
 
     # Prepare the title and header
-    passageReference = passageSoup.select_one(BGW_PASSAGE_TITLE).text.strip()
-    passageVersion = version
+    reference = soup.select_one(BGW_PASSAGE_TITLE).text.strip()
+    version = version
 
     # Remove the unnecessary tags
-    for tag in passageSoup.select(BGW_PASSAGE_IGNORE):
+    for tag in soup.select(BGW_PASSAGE_IGNORE):
         tag.decompose()
 
     # Steps through all the html types and mark these
-    soup = html_utils.stripmd_soup(passageSoup)
-    soup = html_utils.mark_soup(passageSoup, 
+    soup = html_utils.stripmd_soup(soup)
+    soup = html_utils.mark_soup(soup, 
     BGW_PASSAGE_SELECT, html_utils.html_all_tags())
 
-    html_utils.foreach_header(passageSoup, telegram_utils.bold)
+    html_utils.foreach_header(soup, telegram_utils.bold)
 
     # Special formatting for chapter and verse
     for tag in soup.select('.chapternum'):
@@ -69,17 +69,17 @@ def get_passage_raw(ref, version='NIV'):
         tag.string = telegram_utils.italics(telegram_utils.to_sup(tag.text))
 
     # Only at the last step do we do other destructive formatting
-    soup = html_utils.format_soup(soup=passageSoup)
+    soup = html_utils.format_soup(soup)
 
-    passageBlocks = []
+    blocks = []
     for tag in soup(class_=BGW_PASSAGE_SELECT):
-        passageBlocks.append(tag.text)
+        blocks.append(tag.text)
 
-    passageText = telegram_utils.join(passageBlocks, '\n\n')
+    text = telegram_utils.join(blocks, '\n\n')
 
     debug.log("Finished parsing soup")
 
-    return bgw_classes.BGWPassage(passageReference, passageVersion, passageText)
+    return bgw_classes.BGWPassage(reference, version, text)
 
 def get_passage(ref, version='NIV'):
     passage = get_passage_raw(ref, version)
@@ -87,20 +87,20 @@ def get_passage(ref, version='NIV'):
     if passage is None:
         return None
 
-    passageFormat = telegram_utils.bold(passage.get_reference())
-    passageFormat += ' ' + telegram_utils.bracket(passage.get_version())
-    passageFormat += '\n\n' + passage.get_text()
+    text = telegram_utils.bold(passage.get_reference())
+    text += ' ' + telegram_utils.bracket(passage.get_version())
+    text += '\n\n' + passage.get_text()
 
-    return passageFormat
+    return text
 
 def get_reference(query):
     debug.log('Querying for reference ' + query)
 
-    passageSoup = fetch_bgw(query)
-    if passageSoup is None:
+    soup = fetch_bgw(query)
+    if soup is None:
         return None
 
-    reference = passageSoup.select_one(BGW_PASSAGE_TITLE).text
+    reference = soup.select_one(BGW_PASSAGE_TITLE).text
     reference = reference.strip()
 
     return reference
