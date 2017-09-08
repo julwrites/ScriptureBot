54b0d57fd1e62caf1d0b44a4165a88c02641779c
Julian Teh <julian.teh@autodesk.com>
2017-09-08 14:59:23 +0800

    Stripping md

--- dailydevo/cac_utils.py
+++ dailydevo/cac_utils.py
@@ -47,6 +47,7 @@ def get_cacdevo_raw(version='NIV'):
 
     debug.log('Decomposed soup')
     # Steps through all the html types and mark these
+    soup = html_utils.stripmd_soup(soup)
     soup = html_utils.link_soup(soup, telegram_utils.link)
     soup = html_utils.mark_soup(soup, 
     CAC_DEVO_SELECT, html_utils.html_all_tags())
