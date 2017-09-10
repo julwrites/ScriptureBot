9919d82bdc476d97dddf1bde70f4a911f70c2a7d
julwrites <jump4jupiter@gmail.com>
2017-09-10 17:45:01 +0800

    Don't use trycatch

--- common/action/action_utils.py
+++ common/action/action_utils.py
@@ -5,6 +5,20 @@
 from common import debug
 
 def execute(actions, userObj, msg):
+    if userObj is not None:
+        waiting = [action for action in actions if action.waiting(userObj)]
+
+        if len(waiting) == 1:
+            debug.log_action(waiting.identifier())
+            return waiting.resolve(userObj, msg)
+
+        matched = [action for action in actions if action.match(msg)]
+
+        if len(matched) == 1:
+            debug.log_action(matched.identifier())
+            return waiting.resolve(userObj, msg)
+        return False
+
     try:
         if userObj is not None:
             waiting = [action for action in actions if action.waiting(userObj)]
