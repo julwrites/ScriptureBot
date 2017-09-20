0010cefaf6d4e40aba02e1d5aa13ab1154f1a90f
Julian Teh <julian.teh@autodesk.com>
2017-09-20 17:07:44 +0800

    Some fun, making random confirm messages

--- user/user_actions.py
+++ user/user_actions.py
@@ -1,13 +1,24 @@
 
 # coding=utf-8
 
+# Python modules
+import random
+
 # Local modules
 from user import bibleuser_actions
 
 from common.action import action_classes
 from common.telegram import telegram_utils
 
-CONFIRM = "Alright~"
+CONFIRM = [
+    "Alright {}~",
+    "Okay {}~",
+    "Got it, {}~",
+    "Yes, yes, {}~",
+    "I understand, {}~",
+    "If you say so {}~",
+    "Done, {}~",
+]
 
 class UserDoneAction(action_classes.Action):
     def identifier(self):
@@ -17,7 +28,10 @@ class UserDoneAction(action_classes.Action):
         return "Done"
 
     def resolve(self, userObj, msg):
-        telegram_utils.send_close_keyboard(CONFIRM, userObj.get_uid())
+        choose = random.randint(0, len(CONFIRM) - 1)
+        confirmString = CONFIRM[choose].format(userObj.get_name_string())
+
+        telegram_utils.send_close_keyboard(confirmString, userObj.get_uid())
         userObj.set_state(None)
 
         return True
