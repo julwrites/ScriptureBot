99925973950298485001145a8ca76e08ad20d35e
Julian Teh <julian.teh@autodesk.com>
2017-11-09 17:41:40 +0800

    Removing CAC from button access

--- dailydevo/dailydevo_modules.py
+++ dailydevo/dailydevo_modules.py
@@ -3,11 +3,9 @@
 
 # Local modules
 from dailydevo import mcheyne_hooks, mcheyne_actions
-from dailydevo import cac_hooks
 
 def get_actions():
     return mcheyne_actions.get()
 
 def get_hooks():
-    return mcheyne_hooks.get() + \
-            cac_hooks.get()
+    return mcheyne_hooks.get()
