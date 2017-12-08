ce2f2b31382da9adf52110fbfedf469540c58a99
Julian Teh <julian.teh@autodesk.com>
2017-12-08 15:57:05 +0800

    Seemngly no difference, reverting back to new method

--- dailydevo/mcheyne_actions.py
+++ dailydevo/mcheyne_actions.py
@@ -36,8 +36,7 @@ class McheyneDailyAction(action_classes.Action):
 
         if refs is not None:
             refs.append(user_actions.UserDoneAction().name())
-            # options=[telegram_utils.make_button(text=ref) for ref in refs]
-            options = refs
+            options=[telegram_utils.make_button(text=ref) for ref in refs]
 
             telegram_utils.send_msg_keyboard("", userObj.get_uid(), options, 1)
             userObj.set_state(self.identifier())
