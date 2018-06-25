6aebc92aa6ff9f2e40be37eea6d5e7cbd4ceac35
Julian Teh <jump4jupiter@gmail.com>
2018-06-25 23:28:23 +0800

    Adding task for testing and switching to use unittest

--- test.py
+++ test.py
@@ -1,17 +1,54 @@
 # coding=utf-8
 
-from common import debug, web_utils
+import logging
+import unittest
+
+from common import debug, web_utils, text_utils, html_utils
 from common.telegram import telegram_utils
 
+import secret
 
-def test_post():
-    try:
-        telegram_utils.send_msg("Testing")
-    except Exception as e:
-        debug.log(e)
+logging.basicConfig(level=logging.DEBUG)
 
 
-tests = [test_post]
+class test_web_methods(unittest.TestCase):
+    def test_post(self):
+        debug.log("Testing web_utils.http_post")
 
-for test in tests:
-    test()
+        TELEGRAM_URL = "https://api.telegram.org/bot" + secret.BOT_ID
+        TELEGRAM_URL_SEND = TELEGRAM_URL + "/sendMessage"
+        JSON_HEADER = {"Content-Type": "application/json;charset=utf-8"}
+
+        data = {
+            "text": "Testing",
+            "chat_id": text_utils.stringify(secret.BOT_ADMIN),
+            "parse_mode": "Markdown"
+        }
+        debug.log("Performing send: " + text_utils.stringify(str(data)))
+
+        try:
+            web_utils.post_http(TELEGRAM_URL_SEND, data, JSON_HEADER)
+        except Exception as e:
+            self.assertTrue(False, str(e))
+
+
+class test_telegram_methods(unittest.TestCase):
+    def test_send_msg(self):
+        debug.log("Testing telegram_utils.send_msg")
+        try:
+            telegram_utils.send_msg(
+                text="Testing", user=text_utils.stringify(secret.BOT_ADMIN))
+        except Exception as e:
+            self.assertTrue(False, str(e))
+
+
+class test_html_methods(unittest.TestCase):
+    def test_get_url(self):
+        debug.log("Testing html_utils.get_url")
+        try:
+            url, html = html_utils.fetch_html("https://tehj.org")
+        except Exception as e:
+            self.assertTrue(False, str(e))
+
+
+unittest.main()
