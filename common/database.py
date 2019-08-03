b1e10f5a472d879834e1e05f510c75d0ae7ca192
julwrites <julian.teh@outlook.com>
2019-08-03 23:36:11 +0800

    Switching to firebase

--- common/database.py
+++ common/database.py
@@ -1,7 +1,16 @@
 # coding=utf-8
 
-# Google App Engine Modules
-from google.appengine.ext import db
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': project_id,
+})
+
+db = firestore.client()
 
 
 def retrieve(region, name):
