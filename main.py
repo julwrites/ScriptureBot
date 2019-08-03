64d927c505eb8675a1b3e1f0190128333c079ce7
julwrites <julian.teh@outlook.com>
2019-08-03 22:11:27 +0800

    Making bot.py main.py

--- main.py
+++ main.py
@@ -2,19 +2,43 @@
 
 # Python std modules
 import webapp2
+import json
 
-APP_MAIN_URL = "/"
+# Local modules
+from .common import debug_utils, text_utils
+from .common.telegram import telegram_utils
+from .common.action import action_utils
+from .user import user_utils
+
+from . import actions
+
+from secret import BOT_ID
+APP_BOT_URL = "/" + BOT_ID
 
 
-class MainPage(webapp2.RequestHandler):
+class BotHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hi,I'm julwrites")
+        self.post()
+
+    def post(self):
+        data = json.loads(self.request.body)
+        debug_utils.log(data)
+
+        if data.get("message"):
+            msg = data.get("message")
+
+            # Read the user to echo back
+            userId = user_utils.get_uid(msg.get("from").get("id"))
+            userObj = user_utils.get_user(userId)
+
+            if action_utils.execute(actions.get(), userObj, msg):
+                return
+
+            telegram_utils.send_msg(
+                user=msg.get("from").get("id"), text="Hello, I am bot")
 
 
 app = webapp2.WSGIApplication(
     [
-        # (url being accessed, class to call)
-        (APP_MAIN_URL, MainPage),
-    ],
-    debug=True)
+        (APP_BOT_URL, BotHandler),
+    ], debug=True)
