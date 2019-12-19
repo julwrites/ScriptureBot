b514b95de00bedea0ca8d7b896bcc98bae437fda
tehj <julian.teh@autodesk.com>
2019-12-19 16:20:30 +0800

    Stripping out secrets and initial http handling

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package ScriptureBot
 
-// Translator methods
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	http.HandleFunc("/", botHandler)
+// Bot methods
+func HandleBotLogic(props *SessionData) bool {
+	return false
+}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+func telegramHandler(res http.ResponseWriter, req *http.Request, secrets *SecretsData) {
+	env := SessionData{}
+
+	log.Printf("Loading session data...")
+
+	env.Secrets = *secrets
+
+	log.Printf("\tLoaded secrets...")
+
+	if !TranslateToProps(req, &env) {
+		log.Printf("This message was not translatable to bot language")
+		return
 	}
 
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Printf("\tLoaded message...")
+
+	if !HandleBotLogic(&env) {
+		log.Printf("This message was not handled by bot")
+		return
+	}
+
+	if !TranslateToHttp(&env) {
+		log.Printf("This message was not translatable from bot language")
+		return
+	}
 }
