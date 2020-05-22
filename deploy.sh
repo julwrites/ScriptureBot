db221b7bea0d3ba9d963ab33f0d5acb86ac63a8c
julwrites <julian.teh@outlook.com>
2020-05-22 17:42:28 +0800

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo Deploying from $DIR
 
 echo Installing all necessary packages
+go get -u -v -f all
+
+echo Installing app entry package
+# Update and install the entry module
+cd $DIR
+git pull
+go install
+
 # Update and install the sub modules
 cd ../BotSecrets
 git pull
@@ -9,12 +17,7 @@ go install
 cd ../BotMultiplexer
 git pull
 go install
-
-echo Installing app entry package
-# Update and install the entry module
-cd $DIR
-git pull
-go install
+cd ../ScriptureBot
 
 echo Deploying app
 gcloud app deploy --quiet
