5428545ef7fbf069109e5424cf836e2d1a8a3313
Julian Teh <mail@tehj.io>
2023-08-28 00:44:58 +0800

    Small code cleanup

--- pkg/app/admin.go
+++ pkg/app/admin.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/julwrites/BotPlatform/pkg/def"
 	"github.com/julwrites/ScriptureBot/pkg/utils"
 )
@@ -10,8 +12,9 @@ func DumpUserList(env def.SessionData) def.SessionData {
 		var message = ""
 
 		users := utils.GetAllUsers(env.Secrets.PROJECT_ID)
+		message += fmt.Sprintf("%d Users: \n", len(users))
 		for _, user := range users {
-			message += user.Firstname + user.Lastname + " - " + user.Username + "\n"
+			message += user.Firstname + " " + user.Lastname + " - @" + user.Username + "\n"
 		}
 
 		// Retrieve the whole database
